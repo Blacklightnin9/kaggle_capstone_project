c1709dea0afbf6315c2fa7e43ddc2eff63bad56c
Blacklightnin9 <maingame0911@gmail.com>
2025-04-09 19:10:21 +0800

    135 degree

--- Rambutan-Oracle-Cultivating-the-Number/app.py
+++ Rambutan-Oracle-Cultivating-the-Number/app.py
@@ -107,7 +107,7 @@ fig = px.bar(
 #rotate x-axis labels for better readability
 fig.update_layout(
     xaxis=dict(
-        tickangle=65,  # Turns the text vertically
+        tickangle=135,  # Turns the text vertically
         automargin=True
     )
 )
